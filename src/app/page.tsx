f9b802de74e2342e1329b002b373e7753cc74714
exslym <76844562+exslym@users.noreply.github.com>
2025-03-13 13:28:52 +0100

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-gray-800 mb-4">Home</h1>
+    </div>
+  );
+};
+export default Home;
