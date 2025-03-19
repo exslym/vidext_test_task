67e9a681289912bdc0f2bcf3a18df8c8afe9e67d
exslym <76844562+exslym@users.noreply.github.com>
2025-03-19 02:32:29 +0100

    fix: loading & error components

--- src/components/error/Error.tsx
+++ src/components/error/Error.tsx
@@ -1,13 +1,18 @@
 'use client';
 
+import Link from 'next/link';
 import BackButton from '@/components/buttons/BackButton';
 
 export default function Error({ message }: { message?: string }) {
 	return (
-		<div className='absolute inset-0 mx-auto flex max-w-2xl flex-col items-center justify-center gap-3 p-4 text-lg text-red-500'>
-			<p>Error loading editor data!</p>
-			{message && <p className='mb-3'>Details: {message}</p>}
-			<BackButton />
+		<div className='flex min-h-screen w-full flex-col items-center justify-center dark:bg-dark-secondary'>
+			<div className='mx-auto flex h-full max-w-2xl flex-1 flex-col items-center justify-center gap-3 p-4 text-lg text-red-500'>
+				<p>Error loading editor data!</p>
+				{message && <p className='mb-3'>Details: {message}</p>}
+				<Link href='/' className='max-w-fit'>
+					<BackButton />
+				</Link>
+			</div>
 		</div>
 	);
 }
--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -4,9 +4,9 @@ import { Loader2 } from 'lucide-react';
 
 export default function Loading() {
 	return (
-		<div className='absolute inset-0 flex items-center justify-center'>
-			<Loader2 size={32} className='mr-2 animate-spin' />
-			<p className='text-lg'>Loading...</p>
+		<div className='absolute inset-0 flex items-center justify-center dark:bg-dark-secondary'>
+			<Loader2 size={32} className='mr-2 animate-spin dark:text-white' />
+			<p className='text-lg dark:text-white'>Loading...</p>
 		</div>
 	);
 }
